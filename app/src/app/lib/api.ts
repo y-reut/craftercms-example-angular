0fabcaf447f1b123453afca95b3db6da69bee69c
Phil Nguyen <nquangphuong@gmail.com>
2022-02-18 10:50:15 +0700

    Add getModelByUrl

--- app/src/app/lib/api.ts
+++ app/src/app/lib/api.ts
@@ -2,15 +2,26 @@ import {
   getDescriptor,
   parseDescriptor,
   getNavTree,
+  urlTransform,
 } from '@craftercms/content';
-import { firstValueFrom, map } from 'rxjs';
+import { firstValueFrom, map, switchMap } from 'rxjs';
 
 export async function getModel(path = '/site/website/index.xml') {
   return await firstValueFrom(
     getDescriptor(path, { flatten: true }).pipe(
-      map(desc => parseDescriptor(desc))
+      map((desc:any) => parseDescriptor(desc))
       // Can use this for debugging purposes.
       // tap(console.log)
+    )
+  );
+}
+
+export async function getModelByUrl(webUrl = '/') {
+  return await firstValueFrom(
+    urlTransform('renderUrlToStoreUrl', webUrl).pipe(
+      switchMap((path: string) => getDescriptor(path, { flatten: true }).pipe(
+        map((descriptor: any) => parseDescriptor(descriptor, { parseFieldValueTypes: true }))
+      ))
     )
   );
 }
@@ -19,7 +30,7 @@ export async function getNav() {
   return await firstValueFrom(
     getNavTree('/site/website', 1).pipe(
       // Flatten the nav so that home shows at the same level as the children.
-      map((navigation) => [
+      map((navigation: any) => [
         {
           label: navigation.label,
           url: navigation.url,
